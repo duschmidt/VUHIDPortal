42e5e96f85d805e5611660ede6a9707d445659b7
Dustin Schmidt <duschmid@pdx.edu>
2012-07-26 18:51:29 -0700

    GWT hello world

---
+++ VUHIDPortalArtifactId/target/.generated/VUHIDPortalGroupID/client/Messages_.java
@@ -0,0 +1,12 @@
+package VUHIDPortalGroupID.client;
+
+public class Messages_ implements VUHIDPortalGroupID.client.Messages {
+  
+  public java.lang.String nameField() {
+    return "Enter your name";
+  }
+  
+  public java.lang.String sendButton() {
+    return "Send";
+  }
+}
---
+++ VUHIDPortalArtifactId/target/generated-sources/gwt/VUHIDPortalGroupID/client/Messages.java
@@ -0,0 +1,26 @@
+package VUHIDPortalGroupID.client;
+
+/**
+ * Interface to represent the messages contained in resource bundle:
+ * 	/Users/xianthrops/School/CS487/sp2012cs487-teamc/VUHIDPortalArtifactId/target/VUHIDPortalArtifactId-1.0-SNAPSHOT/WEB-INF/classes/VUHIDPortalGroupID/client/Messages.properties'.
+ */
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+  
+  /**
+   * Translated "Enter your name".
+   * 
+   * @return translated "Enter your name"
+   */
+  @DefaultMessage("Enter your name")
+  @Key("nameField")
+  String nameField();
+
+  /**
+   * Translated "Send".
+   * 
+   * @return translated "Send"
+   */
+  @DefaultMessage("Send")
+  @Key("sendButton")
+  String sendButton();
+}
