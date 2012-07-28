1673b92a63b328fe96512a103dc3a2303e4688e8
Damon Liang <damonl@cecs.pdx.edu>
2012-07-28 11:48:34 -0700

    Added Patient class... snarky comment at Dustin

---
+++ VUHIDPortal/Patient.java
@@ -0,0 +1,45 @@
+import java.lang.String;
+
+public class Patient
+{
+   public String First_Name;
+   public String Last_Name;
+   public String VUHID_ID;
+   public String DOB;      //Possible make this into a date
+   
+   public Patient()
+   {
+   }
+   public Patient(String FN, String LN, String Vid, String birthday)
+   {
+      First_Name = FN;
+      Last_Name = LN;
+      VUHID_ID = Vid;
+      DOB = birthday;
+   }
+   public boolean CheckPatient()
+   {
+   
+      if (First_Name == null)
+      {
+         System.out.println("Require First name.");
+         return false;
+      }
+      if (Last_Name == null)
+      {
+         System.out.println("Require Last name.");
+         return false;
+      }
+      if (VUHID_ID == null)
+      {
+         System.out.println("Require VUHID ID.");
+         return false;
+      }
+      if (DOB == null)
+      {
+         System.out.println("Requeire a DOB.");
+         return false;
+      }
+      return true;
+   }
+}
