4424ba4410b5c670b2c7a8a432216231e07ec45a
Dustin Schmidt <duschmid@pdx.edu>
2012-07-31 11:42:26 -0700

    Added incomplete implementaiton of VUHIDSenderInterface called VUHIDSender.  Currently implements only a stub of the getNewOVID method

---
+++ vuhidportal/src/main/java/vuhidtools/VUHIDSender.java
@@ -0,0 +1,8 @@
+package vuhidtools;
+
+public class VUHIDSender implements VUHIDSenderInterface{
+
+	public String   getNewOVID() throws Exception{
+		return "0000003693664829.210573940000000";
+	}
+}
