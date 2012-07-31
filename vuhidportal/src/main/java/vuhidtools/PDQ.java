59082f2cf356a1ae4bafac73b088bf6df5c1f4e0
Dustin Schmidt <duschmid@pdx.edu>
2012-07-31 12:56:31 -0700

    Added beginning of PIX and PDQ implementations

---
+++ vuhidportal/src/main/java/vuhidtools/PDQ.java
@@ -0,0 +1,8 @@
+package vuhidtools;
+
+public class PDQ implements PDQInterface{
+	public Patient[] PatientRegistryFindCandidatesQuery(Patient P){
+		Patient[] p = {new Patient()};
+		return p;
+	};
+}
---
+++ vuhidportal/src/main/java/vuhidtools/PIX.java
@@ -0,0 +1,9 @@
+package vuhidtools;
+
+public class PIX implements PIXInterface{
+
+	public boolean PatientRegistryRecordRevised(Patient P){
+		return true;
+	}
+
+}
