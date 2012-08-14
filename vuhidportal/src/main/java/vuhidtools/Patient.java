8ce4e9d90e3dd7feaba6437f79f7785d51a0192e
Dustin Schmidt <duschmid@pdx.edu>
2012-08-14 12:55:11 -0700

    added stub for static match method which confirms that two input patients match

--- vuhidportal/src/main/java/vuhidtools/Patient.java
+++ vuhidportal/src/main/java/vuhidtools/Patient.java
@@ -18,6 +18,12 @@ public class Patient
       VUHID_ID = Vid;
       DOB = birthday;
    }
+   public static boolean match(Patient P1, Patient P2){
+      // Should return true if all none null fields of P1 (first name, last name, etc) match all non null fields of P2
+      // P1.field != null && P2.field != null and P1.field == P2.field
+      // If the above is true for all fields then the match is true
+      return true;
+   }
    public boolean CheckPatient()
    {
    
