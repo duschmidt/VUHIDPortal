6afe461272c4983af0cd4f754d3608cb454f19e4
Damon Liang <damonl@cecs.pdx.edu>
2012-08-17 01:20:31 -0700

    Made default return values false.
    Default return values are now false, will return true on success.
    Also added code to GetIdentifiersQuery. Need to add more patients to P_List.

--- vuhidportal/src/main/java/vuhidtools/EMPI.java
+++ vuhidportal/src/main/java/vuhidtools/EMPI.java
@@ -10,6 +10,7 @@ public class EMPI implements PIXInterface,PDQInterface
       P_List.add(new Patient("Dustin", "Schmidt", "000001", null));
       P_List.add(new Patient("Peter", "Inslee", "000002", null));
    }
+
 	public boolean PatientRegistryRecordRevised(Patient P){
       if (P == null)
          return false;
@@ -21,35 +22,51 @@ public class EMPI implements PIXInterface,PDQInterface
 		      return true;
          }
       }
-		return true;
+		return false;
 	}
+
    public boolean PatientRegistryRecordAdded(Patient P)
    {
       if (P == null)
          return false;
       return P_List.add(P);
    }
+
    public ArrayList<String> PatientRegistryGetIdentifiersQuery(Patient P)
    {
       ArrayList<String> s = new ArrayList<String>();
+      for (int i = 0; i < P_List.size(); i++)
+      {
+         if (P.match(P_List.get(i)))
+         {
+            //Add IDs
+            s.add(P_List.get(i).VUHID_ID);
+         }
+      }
+      
       return s;
    }
+
    public boolean PatientRegistryDuplicatesResolved(Patient P1, Patient P2)
    {
+      boolean Made_a_Change = false;   //return value
       if (P1 == null || P2 == null)
          return false;
-      int index;
+
+      //Search for matches and then replace them
+      //Currently replaces all matches
       for (int i = 0; i < P_List.size(); i++)
       {
          if (P1.match(P_List.get(i)))
          {
             P_List.set(i, P2);
+            Made_a_Change = true;  //make this line "return true" if you want it to change only one element
          }
       }
-      return true;
+      return Made_a_Change;
    }
 
-   //PDQ
+   //PDQ Function
    public ArrayList<Patient> PatientRegistryFindCandidatesQuery(Patient P) {
       ArrayList<Patient> list = new ArrayList<Patient>();
       for (int i = 0; i < P_List.size(); i++)
