11f2f5adc00c8bdf9d5b1542599533c030c75e84
Robert Hickey <rhickey@cecs.pdx.edu>
2012-08-22 20:21:07 -0700

    Added three test routines for terminateID, getReplacementID, and getDataLocations

--- vuhidportal/src/main/java/vuhidtools/VUHIDPortalPrototype.java
+++ vuhidportal/src/main/java/vuhidtools/VUHIDPortalPrototype.java
@@ -10,6 +10,9 @@ package vuhidtools;
  *                  1.2 Added routine to test getNewOVID routine
  *                      Added routine to test getNewPVID routine
  *                      Added routine to test retireID routine
+ *                  1.3 added routine to test terminateID
+ *                  1.4 added routine to test getReplacementID
+ *                      added routine to test getDataLocations
  *
  */
 public class VUHIDPortalPrototype {
@@ -24,17 +27,17 @@ public class VUHIDPortalPrototype {
     public static void main(String[] args) {
 
         //section to test getStatusOfID routine
-        int temp = 0;
+/*        int temp = 0;
         try {
-            temp = vuhidsend.getStatusOfID("0000000000000009.523079864000000");
+            temp = vuhidsend.getStatusOfID("0000000409177781.095215510000000");
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("\nResponse code for check of ID should be 200; is: " + temp);
+        System.out.println("\nResponse code for check of ID should be 200; is: " + temp);*/
 
 
-/*        //section to test getNewOVID routine
-        String temp = null;
+        //section to test getNewOVID routine
+/*        String temp = null;
         try {
             temp = vuhidsend.getNewOVID();
         } catch (Exception e) {
@@ -43,10 +46,10 @@ public class VUHIDPortalPrototype {
         System.out.println("\nResponse from getNewOVID routine: " + temp);*/
 
 
-/*        //section to test getNewPVID routine
-        String temp = null;
+        //section to test getNewPVID routine
+/*        String temp = null;
         try {
-            temp = vuhidsend.getNewPVID("4000000");
+            temp = vuhidsend.getNewPVID("4000000");     //as per email "4000000" is good for test PC
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -65,75 +68,43 @@ public class VUHIDPortalPrototype {
         System.out.println("HTTP Response from retireID routine, should be 200: " + temp);*/
 
 
-//        try {
-            // create key and trust managers, then initialize the ssl context with their data
-//            SSLSocketFactory factory = am.getSSLSocketFactory();
-            // communicate with the server
-//            URL url = new URL("https://" + config.getVuhidServerHostName() + "/verify/0000000406190821.350181120000000");
-//            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-
-            //OutputStreamWriter  out = null;
-            //BufferedReader      in = null;
-            //String              responseString;
-            //int x;
-
-/* My goal is to start moving all of the following code into the VUHIDSender class.
- * This class should just be a driver that calls methods in VUHIDSender.java. */
-
-//DONE
+        //section to test terminateID routine
+/*        String reason = "OVID was used by CsTheDay only for software testing.";
+        String ID = "0000000000000009.523079864000000"; //just generated this in getNewPVID above
+        int temp = 0;
+        try {
+            temp = vuhidsend.terminateID(ID, reason);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("HTTP Response from terminateID routine, should be 200: " + temp);*/
 
 
-/********************************************************************************************************
- String contentType = connection.getHeaderField("Content-Type");
- String charset = null;
- for (String param : contentType.replace(" ", "").split(";")) {
- if (param.startsWith("charset=")) {
- charset = param.split("=", 2)[1];
- break;
- }
- }
+        //section to test getReplacementID routine
+/*        String temp = null;
+        String reason = "OVID was used by CsTheDay only for software testing.";
+        String ID = "0000000409177781.095215510000000"; //just generated this in getNewPVID above
+        try {
+            temp = vuhidsend.getReplacementID(ID, reason);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("\nResponse from getReplacementID routine: " + temp);*/
 
- if (charset != null) {
- BufferedReader reader = null;
- InputStream response = connection.getInputStream();
- try {
- reader = new BufferedReader(new InputStreamReader(response, charset));
- for (String line; (line = reader.readLine()) != null;) {
- System.out.println(line);
- }
- } finally {
- if (reader != null) try { reader.close(); } catch (IOException logOrIgnore) {}
- }
- } else {
- // It's likely binary content, use InputStream/OutputStream.
- }
- /********************************************************************************************************/
+        //section to test getDataLocations routine
+        String temp[] = null;
+        String ID = "0000000409177781.095215510000000"; //just generated this in getNewPVID above
+        try {
+            temp = vuhidsend.getDataLocations(ID);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        int index = 0;
+        for (String i:temp) {
+            System.out.println("\nResponse from getReplacementID routine: " + temp[index]);
+            index++;
+        }
 
-//            System.out.println("DONE");
 
-/*********************************************************************************************************
-
- connection.setDoOutput(true);
- out = new OutputStreamWriter(connection.getOutputStream());
- out.write("termination_explanation=\"PVID was used by CsTheDay only for software testing.\""); // Remove this for GET requests, or change as needed
- out.close();
- System.out.println("THE RESPONSE CODE IS: " + connection.getResponseCode());
-
- in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
- while ((responseString = in.readLine()) != null) {
- System.out.println(responseString);
- }
- in.close();
- /*********************************************************************************************************
- InputStream error = ((HttpURLConnection) connection).getErrorStream();
- // This was in the original, and may work just fine (or better?), but I haven't tried it yet...
- while ((x = ((InputStream) connection.getContent()).read()) != -1) {
- System.out.print(new String(new byte[] {(byte) x }));
- }
- /*********************************************************************************************************/
-
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }//end main()
 }
